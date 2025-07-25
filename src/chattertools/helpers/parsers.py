4ee944e1b9be0556559eb999f267f6670fa4b7de
kingsmountainlabs <ian@kmlabs.dev>
2025-07-25 09:10:55 -0700

    Update parsers.py to give date time another name

--- src/chattertools/helpers/parsers.py
+++ src/chattertools/helpers/parsers.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime as dt
 from typing import Any, Union
 from result import Result, Ok, Err
 
@@ -12,25 +12,25 @@ class Parse:
             return Err(f"Cannot convert value {val} to integer")
 
     @staticmethod
-    def datetime(val: Union[datetime, int, str]) -> Result[datetime, str]:
+    def datetime(val: Union[dt, int, str]) -> Result[dt, str]:
         if val is None: return Ok(None)
         if val is "": return Ok(None)
 
-        if isinstance(val, datetime): return Ok(val)
-        if isinstance(val, int): return Ok(datetime.fromtimestamp(val))
+        if isinstance(val, dt): return Ok(val)
+        if isinstance(val, int): return Ok(dt.fromtimestamp(val))
 
         if isinstance(val, str):
             formats = ["%Y-%m-%dT%H:%M:%S.%fZ", "%Y-%m-%d %H:%M:%S"]
             for format in formats:
                 try:
-                    return Ok(datetime.strptime(val, format))
+                    return Ok(dt.strptime(val, format))
                 except ValueError:
                     pass
 
             return Err(f'Invalid datetime format "{val}"')
     
     @staticmethod
-    def apiDatetime(val: datetime) -> Result[str, str]:
+    def apiDatetime(val: dt) -> Result[str, str]:
         try:
             return Ok(val.strftime("%Y-%m-%d %H:%M:%S"))
         except Exception:
